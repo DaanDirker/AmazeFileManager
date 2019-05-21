99e92f30c556bdad25fc335f8aa3089e8987266d
Unknown <khaled_t23@hotmail.com>
2019-05-22 00:02:16 +0200

    Few changes

--- app/src/androidTest/java/PermissionsTest.java
+++ app/src/androidTest/java/PermissionsTest.java
@@ -1,5 +1,3 @@
-import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.test.InstrumentationRegistry;
@@ -8,26 +6,19 @@ import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.app.ActivityCompat;
 
-import com.amaze.filemanager.R;
 import com.amaze.filemanager.activities.MainActivity;
-import com.amaze.filemanager.activities.superclasses.PermissionsActivity;
-import com.amaze.filemanager.fragments.MainFragment;
-import com.amaze.filemanager.fragments.TabFragment;
-import com.amaze.filemanager.utils.MainActivityHelper;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class PermissionsTest {
 
-    Context context;
-
+    private Context context;
 
     @Rule
     public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
@@ -35,15 +26,13 @@ public class PermissionsTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class, false, true);
 
-
     @Before
     public void beforeTesting() {
-        context = InstrumentationRegistry.getTargetContext();
-    }
+        context = InstrumentationRegistry.getTargetContext(); }
 
     @Test
-    public void testPermissions() throws Throwable {
-        assertTrue(String.valueOf(true), ActivityCompat.checkSelfPermission((Activity) context, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    public void testPermissions() {
+        assertTrue(String.valueOf(true), ActivityCompat.checkSelfPermission(context, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 == PackageManager.PERMISSION_GRANTED);
     }
 }
--- app/src/androidTest/java/SearchFileTest.java
+++ app/src/androidTest/java/SearchFileTest.java
@@ -1,43 +1,38 @@
-import android.content.Context;
-import android.os.Environment;
-import android.preference.PreferenceManager;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.amaze.filemanager.R;
 import com.amaze.filemanager.activities.MainActivity;
 import com.amaze.filemanager.filesystem.HybridFileParcelable;
 
 import org.junit.Before;
-import org.junit.*;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class SearchFileTest {
 
-    private HybridFileParcelable filePath;
+    private HybridFileParcelable fileOne;
+    private HybridFileParcelable fileTwo;
+    private HybridFileParcelable fileThree;
+    
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class, false, false);
 
     @Before
     public void beforeTesting()  {
-        filePath = new HybridFileParcelable("/storage/sdcard0/TestDirectory/TestFile.txt");
+        fileOne = new HybridFileParcelable("/storage/sdcard0/TestDirectory/TestFile.txt");
+        fileTwo = new HybridFileParcelable("/storage/sdcard0/TestDirectory/TestFile2.txt");
+        fileThree = new HybridFileParcelable("/storage/sdcard0/TestDirectory/TestFile3.txt");
+
     }
 
     @Test
     public void testMethod() {
-        assertEquals("TestFile.txt", filePath.getName());
+        assertEquals("TestFile.txt", fileOne.getName());
+        assertEquals("TestFile2.txt", fileTwo.getName());
+        assertEquals("TestFile3.txt", fileThree.getName());
     }
 }
